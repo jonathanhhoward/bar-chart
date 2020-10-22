fc6118b94e7232c5400152d8e434e112cdf66f31
jonathanhhoward <2773262@gmail.com>
2020-10-22 01:23:20 -0500

    Use scaleBand for bars

--- main.js
+++ main.js
@@ -16,10 +16,10 @@ function barChart(dataset) {
   const margin = { top: 50, right: 50, bottom: 50, left: 100 };
   const chartWidth = svgWidth - margin.left - margin.right;
   const chartHeight = svgHeight - margin.top - margin.bottom;
-  const barWidth = (chartWidth / dataset.length) * 0.8;
 
-  const xScale = d3.scaleTime()
-    .domain(d3.extent(dataset, d => new Date(d.date)))
+  const xScale = d3.scaleBand()
+    .domain(dataset.map(d => new Date(d.date)))
+    .padding(0.2)
     .range([0, chartWidth]);
 
   const yScale = d3.scaleLinear()
@@ -42,10 +42,22 @@ function barChart(dataset) {
   const chart = svg.append('g')
     .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
+  const xTicks = dataset.filter(d => {
+      const date = new Date(d.date);
+      const year = +d3.utcFormat('%Y')(date);
+      const month = d3.utcFormat('%B')(date);
+      return year % 5 === 0 && month === 'January';
+    })
+    .map(d => new Date(d.date));
+
+  const xAxis = d3.axisBottom(xScale)
+    .tickValues(xTicks)
+    .tickFormat(d3.utcFormat('%Y'));
+
   chart.append('g')
     .attr('id', 'x-axis')
     .attr('transform', `translate(0, ${chartHeight})`)
-    .call(d3.axisBottom(xScale));
+    .call(xAxis);
 
   chart.append('g')
     .attr('id', 'y-axis')
@@ -58,7 +70,7 @@ function barChart(dataset) {
     .attr('class', 'bar')
     .attr('x', d => xScale(new Date(d.date)))
     .attr('y', d => yScale(d.gdp))
-    .attr('width', barWidth)
+    .attr('width', xScale.bandwidth())
     .attr('height', d => yScale(0) - yScale(d.gdp))
     .attr('data-date', d => d.date)
     .attr('data-gdp', d => d.gdp)
